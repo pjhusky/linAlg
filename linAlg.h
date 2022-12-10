88559148f85efd61c86a4ec770710cb62f39df45
P H <p.houska@cgv.tugraz.at>
2022-12-10 09:41:27 +0100

    operators '*' and '==' for mat and vec (also 3x4 matrix multiplication)

--- linAlg.h
+++ linAlg.h
@@ -205,12 +205,16 @@ struct linAlg {
 
 // for matrix: inner array stores the rows vectors, outer array says how many row vectors we have
 // vector = matrix * vector
-template<typename val_T, size_t numColumns_T, size_t numRows_T>
-static std::array<val_T, numRows_T> operator*( const std::array< std::array<val_T, numColumns_T>, numRows_T >& matrix, const std::array<val_T, numColumns_T>& vector ) {
-    std::array<val_T, numRows_T> result;
-    result[0] = linAlg::dot( matrix[0], vector );
-    result[1] = linAlg::dot( matrix[1], vector );
-    result[2] = linAlg::dot( matrix[2], vector );
+template<typename val_T, std::size_t numColumns_T, std::size_t numRows_T>
+static linAlg::vec_t<val_T, numRows_T> operator*( const linAlg::mat_t<val_T, numRows_T, numColumns_T >& matrix, 
+                                                  const linAlg::vec_t<val_T, numColumns_T>& vector ) {
+    linAlg::vec_t<val_T, numRows_T> result;
+    // result[0] = linAlg::dot( matrix[0], vector );
+    // result[1] = linAlg::dot( matrix[1], vector );
+    // result[2] = linAlg::dot( matrix[2], vector );
+    for ( std::size_t row = 0; row < numRows_T; row++ ) {
+        result[row] = linAlg::dot( matrix[row], vector );
+    }
     return result;
 }
 
@@ -219,18 +223,69 @@ static std::array<val_T, numRows_T> operator*( const std::array< std::array<val_
 // "inner" index - the one closer to the var name - accesses the row, the outer "last" indexing parentheses index the column within that row
 template<typename val_T, std::size_t rowsL_T, std::size_t colsL_rowsR_T, std::size_t colsR_T>
 static linAlg::mat_t<val_T, rowsL_T, colsR_T > operator* ( const linAlg::mat_t< val_T, rowsL_T, colsL_rowsR_T >& matrixLhs,
-    const linAlg::mat_t< val_T, colsL_rowsR_T, colsR_T >& matrixRhs ) {
-    linAlg::mat_t < val_T, rowsL_T, colsR_T > result;
+                                                           const linAlg::mat_t< val_T, colsL_rowsR_T, colsR_T >& matrixRhs ) {
+    linAlg::mat_t< val_T, rowsL_T, colsR_T > result;
     for (std::size_t col = 0; col < colsR_T; col++) { // left columns
         for (std::size_t row = 0; row < rowsL_T; row++) { // right rows
             float accum = 0.0f;
             for (std::size_t k = 0; k < colsL_rowsR_T; k++) { // dot of left row (over columns) and right columns ( over rows )
-                accum += matrixLhs.data[row][k] * matrixRhs.data[k][col];
+                accum += matrixLhs[row][k] * matrixRhs[k][col];
             }
-            result.data[row][col] = accum;
+            result[row][col] = accum;
         }
     }
     return result;
 }
+
+// e.g., in 3D, non-perspective transformations can be represented by 3x4 matrices => treat them as 3x3 rotations and fix up translational part
+template<typename val_T, std::size_t rowsL_T>
+static linAlg::mat_t<val_T, rowsL_T, rowsL_T+1 > operator* ( const linAlg::mat_t< val_T, rowsL_T, rowsL_T+1 >& matrixLhs,
+                                                             const linAlg::mat_t< val_T, rowsL_T, rowsL_T+1 >& matrixRhs ) {
+    constexpr std::size_t colsR_T = rowsL_T+1;
+    linAlg::mat_t< val_T, rowsL_T, colsR_T > result;
+    for (std::size_t col = 0; col < rowsL_T; col++) { // left columns
+        for (std::size_t row = 0; row < rowsL_T; row++) { // right rows
+            float accum = 0.0f;
+            for (std::size_t k = 0; k < colsR_T; k++) { // dot of left row (over columns) and right columns ( over rows )
+                accum += matrixLhs[row][k] * matrixRhs[k][col];
+            }
+            result[row][col] = accum;
+        }
+    }
+    // fix-up for last column (translations)
+    // result[0][3] = matrixLhs[0][3];
+    // result[1][3] = matrixLhs[1][3];
+    // result[2][3] = matrixLhs[2][3];
+    constexpr std::size_t lastColumnIdx = colsR_T - 1;
+    for ( std::size_t row = 0; row < rowsL_T; row++ ) {
+        result[row][lastColumnIdx] = matrixLhs[row][lastColumnIdx];
+    }
+    return result;
+}
+
+template<typename val_T, std::size_t numCoordsL_T, std::size_t numCoordsR_T>
+static bool operator==( const linAlg::vec_t<val_T, numCoordsL_T>& vectorLhs,
+                        const linAlg::vec_t<val_T, numCoordsR_T>& vectorRhs ) {
+    if ( numCoordsL_T != numCoordsR_T ) { return false; }
+    for ( std::size_t coord = 0; coord < numCoordsL_T; coord++ ) {
+        if ( vectorLhs[coord] != vectorRhs[coord] ) { return false; }
+    }
+    return true;
+}
+
+template<typename val_T, std::size_t rowsL_T, std::size_t colsL_T, std::size_t rowsR_T, std::size_t colsR_T>
+static bool operator== ( const linAlg::mat_t< val_T, rowsL_T, colsL_T >& matrixLhs,
+                         const linAlg::mat_t< val_T, rowsR_T, colsR_T >& matrixRhs ) {
+    if ( rowsL_T != rowsR_T || colsL_T != colsR_T ) { return false; }
+    
+    for (std::size_t col = 0; col < rowsL_T; col++) {
+        for (std::size_t row = 0; row < rowsL_T; row++) {
+            if ( matrixLhs[row][col] != matrixRhs[row][col] ) { return false; }
+        }
+    }
+    
+    return true;
+}
+
 
 #endif // _LINALG_H_671dfc89_7e49_4a11_812b_1b04a6e746cc
